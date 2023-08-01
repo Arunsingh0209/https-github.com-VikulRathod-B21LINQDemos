17e7f057ba7f11734a721fe1738ea2cb795a8e16
vikul rathod <vikul.rathod.net@gmail.com>
2023-08-01 13:11:27 +0530

    Add project files.

---
+++ 1_WhyLinq/Program.cs
@@ -0,0 +1,67 @@
+﻿#region regular C# code
+
+List<Student> students = new List<Student>()
+{
+new Student(){ RollNumber = 1, Name = "Vishal", Gender  = "Male"},
+new Student(){ RollNumber = 2, Name = "Shital", Gender  = "Female"},
+new Student(){ RollNumber = 3, Name = "Vivek", Gender  = "Male"},
+new Student(){ RollNumber = 4, Name = "Priya", Gender  = "Female"},
+new Student(){ RollNumber = 5, Name = "Dipak", Gender  = "Male"}
+};
+
+foreach (Student s in students)
+{
+    Console.WriteLine($"Roll Number : {s.RollNumber} Name : {s.Name} Gender : {s.Gender}");
+}
+
+// filter female students
+//List<Student> femaleStudents = new List<Student>();
+//foreach (Student s in students)
+//{
+//    if (s.Gender.Equals("Female"))
+//    {
+//        femaleStudents.Add(s);
+//    }
+//}
+
+// linq query syntax
+//var femaleStudents = from s in students
+//                     where s.Gender.Equals("Female")
+//                     select s;
+
+var femaleStudents = students.Where(s => s.Gender.Equals("Female"));
+
+Console.WriteLine("*** Female Students ***");
+foreach (Student s in femaleStudents)
+{
+    Console.WriteLine($"Roll Number : {s.RollNumber} Name : {s.Name} Gender : {s.Gender}");
+}
+
+//List<int> numbers1 = new List<int>();
+//HashSet<int> numbers2 = new HashSet<int>();
+
+var names = students.Select(s => s.Name);
+foreach (string n in names)
+{
+    Console.Write($"{n} ");
+}
+
+Console.WriteLine();
+
+List<object> list = new List<object>() { 10, 20, "Vishal", '$', 20.5f, '#'};
+var result = list.OfType<char>();
+foreach (char item in result)
+{
+    Console.Write($"{item} ");
+}
+
+#endregion regular C# code
+
+Console.ReadLine();
+
+class Student
+{
+    public int RollNumber { get; set; }
+    public string Name { get; set; }
+    public string Gender { get; set; }
+}
---
+++ 2_ExtensionMethods/Program.cs
@@ -0,0 +1,78 @@
+﻿Customer c = new Customer();
+c.PrintA();
+c.PrintB();
+
+string name = "Vishal";
+string result = name.MeLihileliReverseMethod();
+Console.WriteLine($"Input: {name} Output: {result}");
+string email = "vivek@gmail.com";
+string remail = email.MeLihileliReverseMethod();
+Console.WriteLine($"Input: {email} Output: {remail}");
+
+A a1 = new A();
+a1.PrintTypeName();
+
+B b1 = new B();
+b1.PrintTypeName();
+
+//Customer1 c1 = new Customer1();
+//c1.PrintB();
+//c1.PrintA();
+
+Console.ReadLine();
+
+public sealed class Customer
+{
+    public void PrintA()
+    {
+        Console.WriteLine("A");
+    }
+}
+
+//class Customer1 : Customer
+//{
+//    public void PrintB()
+//    {
+//        Console.WriteLine("B");
+//    }
+//}
+
+public class A : IA { }
+public class B : IA{ }
+
+public interface IA { }
+
+public static class CustomerHelper
+{
+    public static void PrintB(this Customer c)
+    {
+        Console.WriteLine("B");
+    }
+
+    public static string MeLihileliReverseMethod(this string name)
+    {
+        string result = "";
+        for (int i = name.Length - 1; i >= 0; i--)
+        {
+            result += name[i];
+        }
+
+        return result;
+    }
+
+    //public static void PrintTypeName(this A a)
+    //{
+    //    Console.WriteLine($"Type name : {a.GetType().Name}");
+    //}
+
+    //public static void PrintTypeName(this B a)
+    //{
+    //    Console.WriteLine($"Type name : {a.GetType().Name}");
+    //}
+
+    public static void PrintTypeName(this IA a)
+    {
+        Console.WriteLine($"Type name : {a.GetType().Name}");
+    }
+}
+
